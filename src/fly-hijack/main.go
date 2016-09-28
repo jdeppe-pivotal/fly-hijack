c16e36542cb816a367c1ed2ac3e13a1baf36379e
Jens Deppe <jdeppe@pivotal.io>
2016-09-28 06:36:42 -0700

    Initial commit

---
+++ src/fly-hijack/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+	"fmt"
+	"syscall"
+	"os"
+)
+
+const (
+	PIPELINE = 4
+	JOB      = 6
+	BUILD    = 8
+)
+
+func main() {
+	var instance string
+
+	flag.StringVar(&instance, "t", "", "The concourse instance name")
+	flag.Parse()
+
+	if instance == "" {
+		log.Fatal("Please provide an instance with '-t'")
+	}
+
+	// Ex: http://concourse.gemfire.pivotalci.info:8080/pipelines/gemfire-9.0.0/jobs/OperationsTest/builds/4
+	parts := strings.Split(flag.Arg(0), "/")
+
+	args := []string{
+		"fly",
+		"-t", instance,
+		"hijack",
+		"-j", fmt.Sprintf("%s/%s", parts[PIPELINE], parts[JOB]),
+		"-b", parts[BUILD],
+	}
+
+	err := syscall.Exec("/usr/local/bin/fly", args, os.Environ())
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to launch fly: %s", err))
+	}
+}
