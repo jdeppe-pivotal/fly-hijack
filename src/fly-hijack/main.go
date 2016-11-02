c570d7f4c19f0482058d847dd19f122d64eecc3e
Jens Deppe <jdeppe@pivotal.io>
2016-11-02 07:29:47 -0700

    Provide ability to infer target from URL
    - Now using -t is optional. The target is inferred by matching the given URL
      with what is in ~/.flyrc.

--- src/fly-hijack/main.go
+++ src/fly-hijack/main.go
@@ -7,12 +7,17 @@ import (
 	"fmt"
 	"syscall"
 	"os"
+	"errors"
+	"path"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"net/url"
 )
 
 const (
-	PIPELINE = 4
-	JOB      = 6
-	BUILD    = 8
+	PIPELINE = 3
+	JOB      = 5
+	BUILD    = 7
 )
 
 func main() {
@@ -21,13 +26,24 @@ func main() {
 	flag.StringVar(&instance, "t", "", "The concourse instance name")
 	flag.Parse()
 
-	if instance == "" {
-		log.Fatal("Please provide an instance with '-t'")
+	u, err := url.Parse(flag.Arg(0))
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to parse URL: %s", err))
 	}
 
-	// Ex: http://concourse.gemfire.pivotalci.info:8080/pipelines/gemfire-9.0.0/jobs/OperationsTest/builds/4
-	parts := strings.Split(flag.Arg(0), "/")
+	// Ex: /teams/main/pipelines/gemfire-9.0.0/jobs/OperationsTest/builds/4
+	parts := strings.Split(u.Path, "/")
 
+	// We didn't give -t option
+	if instance == "" {
+		hostPart := fmt.Sprintf("%s://%s", u.Scheme, u.Host)
+
+		instance, err = getTarget(hostPart)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	
 	args := []string{
 		"fly",
 		"-t", instance,
@@ -35,12 +51,47 @@ func main() {
 		"-j", fmt.Sprintf("%s/%s", parts[PIPELINE], parts[JOB]),
 	}
 
-        if len(parts) > BUILD {
+	if len(parts) > BUILD {
 		args = append(args, "-b", parts[BUILD])
-        }
+	}
 
-	err := syscall.Exec("/usr/local/bin/fly", args, os.Environ())
+	err = syscall.Exec("/usr/local/bin/fly", args, os.Environ())
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Unable to launch fly: %s", err))
 	}
+}
+
+type Flyrc struct {
+	Targets map[string]flyrcTarget `yaml:"targets"`
+}
+
+type flyrcTarget struct {
+	Api string `yaml:"api"`
+}
+
+func getTarget(host string) (string, error) {
+	home := os.Getenv("HOME")
+	if home == "" {
+		return "", errors.New("Unable to determine users' HOME - is $HOME set?")
+	}
+
+	flyrcData, err := ioutil.ReadFile(path.Join(home, ".flyrc"))
+	if err != nil {
+		return "", err
+	}
+
+	f := Flyrc{}
+	err = yaml.Unmarshal(flyrcData, &f)
+	if err != nil {
+		return "", err
+	}
+
+
+	for k, v := range f.Targets {
+		if v.Api == host {
+			return k, nil
+		}
+	}
+
+	return "", errors.New("Unable to match URL with target in ~/.flyrc")
 }
